dcf71fb07cf2c07b444ea119ce4e59f001f6bb47
pmm <miaomiaopan@163.com>
2025-03-21 08:57:56 +0800

    添加用于保存core-dump的代码

--- src/slam_main.cpp
+++ src/slam_main.cpp
@@ -1,20 +1,49 @@
 #include <ros/ros.h>
 #include "node/localization_module.h"
 
+#include <signal.h>
+#include <sys/resource.h>
+#define SAVE_CORE_DUMP
+
+#define CORE_SIZE 1024 * 1024 * 500 * 1.2
+
 
 int main(int argc,char **argv){
+#ifdef SAVE_CORE_DUMP
+    ROS_INFO("save core dump is enable");
+    // 程序崩溃核心转储
+    struct rlimit rlmt;
+    if (getrlimit(RLIMIT_CORE, &rlmt) == -1) {
+        return -1;
+    }
+    ROS_INFO(
+        "Before set rlimit CORE dump current is:%d, max is:%d", (int)rlmt.rlim_cur,
+        (int)rlmt.rlim_max);
+
+    rlmt.rlim_cur = (rlim_t)CORE_SIZE;
+    rlmt.rlim_max = (rlim_t)CORE_SIZE;
+    if (setrlimit(RLIMIT_CORE, &rlmt) == -1) {
+        return -1;
+    }
+    if (getrlimit(RLIMIT_CORE, &rlmt) == -1) {
+        return -1;
+    }
+    ROS_INFO(
+        "After set rlimit CORE dump current is:%d, max is:%d", (int)rlmt.rlim_cur,
+        (int)rlmt.rlim_max);
+    //! core缓存数设为三个 运行roslaunch一般保存在 $HOME/.ros/下
+    std::string HOME(getenv("HOME"));
+    // if (boost::filesystem::exists(HOME + "/.ros/core-decision_planni")) {
+    if (boost::filesystem::exists(HOME + "/.ros/core-lidar_slam_node")) {
+        int re = system("rm core-lidar_slam_node");
+    }
+#else
+    ROS_INFO("save core dump is disable");
+#endif
 
     ros::init(argc, argv, "localization_module");
     ros::NodeHandle nh;
     std::string curr_path;
-    // #ifdef CURRENT_DIR
-    //     // std::cout<<"CURRENT_DIR is defined"<<std::endl;
-    //     ROS_INFO("CURRENT_DIR is defined");
-    //     curr_path = CURRENT_DIR;
-    // #else
-    //     // std::cout<<"CURRENT_DIR is not defined"<<std::endl;
-    //     ROS_WARN("CURRENT_DIR is not defined");
-    // #endif
     
     // 设置locale为默认值，以支持当前系统的默认编码
     setlocale(LC_ALL, "");
@@ -25,13 +54,10 @@ int main(int argc,char **argv){
     localization_module::ModuleStatus init_status = static_cast<localization_module::ModuleStatus>(init_module_status);
     
 
-    localization_module::LocalizationModule localization_module(/*curr_path,*/ init_status);
+    ROS_INFO("\033[1;32m----> localization_module starting! \033[0m");
+    localization_module::LocalizationModule localization_module(init_status);
     
-    ROS_INFO("\033[1;32m----> localization_module start! \033[0m");
 
-    // ros::MultiThreadedSpinner spinner(10);
-    // spinner.spin();
-    // ros::spin();
     return 0;
 }
 
