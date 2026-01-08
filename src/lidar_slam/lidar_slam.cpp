4f168c64e07c657e4cc5880e282602246dd40818
jiaoxiaoliang <18991971519@163.com>
2026-01-08 19:53:25 +0800

    style: 扩大lio异常速度检测阈值

--- src/lidar_slam/lidar_slam.cpp
+++ src/lidar_slam/lidar_slam.cpp
@@ -1091,9 +1091,9 @@ bool LidarSlam::check_lio_vel_abnormal(const state_ikfom& imu_state) {
 	const auto& vx = body_vel.x();
 	const auto& vy = body_vel.y();
 	const auto& vz = body_vel.z();
-	double vx_thresh = 2.0;
+	double vx_thresh = 3.0;
 	double vy_thresh = 2.0;
-	double vz_thresh = 1.0;
+	double vz_thresh = 2.0;
 	if (std::fabs(vx) >= vx_thresh || std::fabs(vy) >= vy_thresh || std::fabs(vz) >= vz_thresh) {
 		TRACE_WARN_CLASS(
 			"abnormal lidar velocity, vx: %.2f >= thresh = %f, vy: %.2f >= thresh = %f, vz: %.2f >= thresh = %f", vx,
